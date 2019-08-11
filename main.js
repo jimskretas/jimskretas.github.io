ff232b4d177d65e57ce2ff675b5f41fe25a63548
Jim Skretas <jimskretas@gmail.com>
2019-08-11 21:45:24 +0300

    Add carousel and fixes

--- main.js
+++ main.js
@@ -39,4 +39,31 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
+  $('.carousel-items').slick({
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
 })(jQuery); // End of use strict
+	
